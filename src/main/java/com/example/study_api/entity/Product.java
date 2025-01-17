dacf7b5ee35684f453b919324b1abf802579cdac
hanjihoon03 <gkswlgns7653@gmail.com>
2025-01-17 23:02:12 +0900

    jwtutil 및 엔티티생성

---
+++ src/main/java/com/example/study_api/entity/Product.java
@@ -0,0 +1,29 @@
+package com.example.study_api.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String title;
+
+    private String link;
+    private String image;
+    private String brand;
+    private int lprice;
+    private int hprice;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+}
--- src/main/java/com/example/study_api/entity/User.java
+++ src/main/java/com/example/study_api/entity/User.java
@@ -1,4 +1,40 @@
 package com.example.study_api.entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotEmpty
+    @Column(nullable = false)
+    private String username;
+
+    @Email
+    @Column(nullable = false)
+    private String email;
+
+    @NotEmpty
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    @Enumerated(value = EnumType.STRING)
+    private UserRoleEnum role;
+
+    @OneToMany(mappedBy = "user")
+    private List<Product> products = new ArrayList<>();
 }
